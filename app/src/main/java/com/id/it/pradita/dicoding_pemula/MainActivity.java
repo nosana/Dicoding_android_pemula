98c28cedb61b71943891c62d457f286cbb6c94c6
nosana <novanopan26@gmail.com>
2018-04-13 11:16:28 +0700

    Dicoding Pemula (satu)

---
+++ app/src/main/java/com/id/it/pradita/dicoding_pemula/MainActivity.java
@@ -0,0 +1,13 @@
+package com.id.it.pradita.dicoding_pemula;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
